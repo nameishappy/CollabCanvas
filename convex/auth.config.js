3f824d33d2b776b7903c975af71e2cc12f39ae80
nameishappy <105534032+nameishappy@users.noreply.github.com>
2024-08-12 11:37:26 +0530

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://liberal-elk-45.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
